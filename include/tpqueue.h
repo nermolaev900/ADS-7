4b1913337ecb835e4253f7a38fdd3416815570fd
nermolaev900 <85300440+nermolaev900@users.noreply.github.com>
2022-06-10 17:23:42 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,10 +1,59 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2022 Nikita Ermolaev
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
+template <typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+ private:
+  struct ITEM {
+    T val;
+    ITEM* next;
+    ITEM* prev;
+  };
+  TPQueue::ITEM* create(const T& val) {
+    ITEM* item = new ITEM;
+    item->val = val;
+    item->next = item->prev = nullptr;
+    return item;
+  }
+  ITEM* tail;
+  ITEM* head;
+
+ public:
+  TPQueue() : head(nullptr), tail(nullptr) {}
+  void push(const T& val) {
+    ITEM* cur = head;
+    ITEM* item = create(val);
+    while (cur && cur->val.prior >= val.prior)
+      cur = cur->next;
+    if (!cur && head) {
+      tail->next = item;
+      item->prev = tail;
+      tail = item;
+      return;
+    } else if (!cur && !head) {
+      head = tail = item;
+    } else if (!cur->prev) {
+      head->prev = item;
+      item->next = head;
+      head = item;
+    } else {
+      cur->prev->next = item;
+      item->prev = cur->prev;
+      item->next = cur;
+      cur->prev = item;
+    }
+  }
+  T pop() {
+    if (!head || !tail) throw "Empty";
+    ITEM* temp = head->next;
+    if (temp) temp->prev = nullptr;
+    T val = head->val;
+    delete head;
+    head = temp;
+    if (!head) tail = nullptr;
+    return val;
+  }
 };
 
 struct SYM {
